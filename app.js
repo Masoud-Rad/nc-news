786d39551c8f1b647a58253a32da810a806a482c
Mas <mas@Mass-MacBook-Pro.local>
2023-05-10 14:15:43 +0100

    task 4 finish

--- app.js
+++ app.js
@@ -2,20 +2,35 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const {getDescription, getTopics , getArticles } = require ("./controllers/api.controllers")
+const {getDescription, getTopics , getArticles, getArticlesById } = require ("./controllers/api.controllers")
 
 
 
 app.get('/api', getDescription);
 
 app.get("/api/topics",getTopics);
+
 app.get("/api/articles",getArticles);
 
+app.get(`/api/articles/:article_id`,getArticlesById);
+
+app.use((error, req, res, next) => {
+    if(error.code==="22P02")
+    {
+        res.status(404).send({ msg: "Not Found!" })
+    }else{
+        next(error)
+    }
+    
+})
 app.all("*", (req,res)=>{
-    res.status(404).send({ msg: "NOT FOUND!" })
+    res.status(404).send({ msg: "Not Found!" })
 })
 app.use((error, req, res, next) => {
     res.status(error.status).send({ msg: error.msg })
 })
 
+app.use((error, req, res, next) => {
+    res.status(500).send({ msg: "Server Error!" })
+})
 module.exports = app 
