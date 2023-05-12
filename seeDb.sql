1ba87c83c2cd39a53ba5e432d0eb3942691ba6ec
Masoud <masoud.rad.uk@gmail.com>
2023-05-12 16:10:51 +0100

    task 11 is done and updated Delete comment error handeling

--- seeDb.sql
+++ seeDb.sql
@@ -1,9 +1,14 @@
 \c nc_news_test
 
+SElECT * FROM articles
+--
+;
 
 
-
-  SELECT 
-       *
-        FROM articles 
-        ;
+SELECT 
+    articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes,articles.article_img_url,
+    COUNT(comments.comment_id) as comment_count
+    FROM articles 
+    LEFT JOIN comments
+    ON comments.article_id=articles.article_id
+    WHERE articles.topic = 'mitch' GROUP BY articles.article_id ORDER BY title ASC;
